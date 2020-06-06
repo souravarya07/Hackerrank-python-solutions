dadfde22606a927b88da82663e09ae892929155a
Sourav arya <souravarya07@gmail.com>
2020-06-06 20:31:37 +0530

    python solutions

---
+++ print function.py
@@ -0,0 +1,30 @@
+# Check Tutorial tab to know how to to solve.
+#
+# Read an integer .
+#
+# Without using any string methods, try to print the following:
+#
+#
+# Note that "" represents the values in between.
+#
+# Input Format
+#
+# The first line contains an integer .
+#
+# Output Format
+#
+# Output the answer as explained in the task.
+#
+# Sample Input 0
+#
+# 3
+# Sample Output 0
+#
+# 123
+#----------------------------------------------------------
+#Solution:
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i, end='')
